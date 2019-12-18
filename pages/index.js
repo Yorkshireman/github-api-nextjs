ce0cb0e8c3af6e4028ec4b8ad5f2088c8ffd0406
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-12-18 23:28:29 +0000

    Setup a basic search form

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,56 @@
-import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input
+} from 'reactstrap';
+import React, { useState } from 'react';
 
-const Index = () => (
-  <div>
-    <Head>
-      <title>GitHub API Tool</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta charSet='utf-8' />
-    </Head>
-    <p>Hello!</p>
-  </div>
-);
+import HeadComponent from '../components/Head';
+
+const Index = () => {
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    name === 'username' ? setUsername(value) : null;
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    setLoading(true);
+    console.log('call service, username: ', username);
+  };
+
+  return (
+    <main className='container'>
+      <HeadComponent />
+      <section>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Input
+              id='username'
+              name='username'
+              onChange={handleChange}
+              placeholder='Github username'
+              type='text'
+              value={username}
+            />
+          </FormGroup>
+          { loading ?
+            <p>Loading...</p> :
+            <Button color='primary'>Search</Button>
+          }
+        </Form>
+      </section>
+      <style jsx>{`
+        main {
+          padding-top: 1em;
+        }
+      `}</style>
+    </main>
+  );
+};
 
 export default Index;
