9c00c717aaf2d7006fbbec9e0987f6286fcfa363
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-12-18 22:32:30 +0000

    add Head

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Head from 'next/head';
+
 const Index = () => (
   <div>
+    <Head>
+      <title>GitHub API Tool</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta charSet='utf-8' />
+    </Head>
     <p>Hello!</p>
   </div>
 );
